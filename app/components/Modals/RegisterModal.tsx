0b11d04a58ddfa9d91ec72e864ab609e9341eda5
dennohkim <kimathidennis38@gmail.com>
2023-04-06 12:25:16 +0300

    feat: implement login with github

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -10,6 +10,7 @@ import Heading from "../Heading";
 import Input from "../inputs/Input";
 import toast from "react-hot-toast";
 import Button from "../Button";
+import { signIn } from "next-auth/react";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -87,14 +88,18 @@ const RegisterModal = () => {
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn("github")}
       />
       <div className="text-neutral-500 text-center mt-4 font-light ">
         <div className="flex flex-row justify-center items-center gap-2">
           <div>Already have an account?</div>
-          <div onClick={registerModal.onClose} className="text-neutral-800 cursor-pointer hover:underline">Log in</div>
+          <div
+            onClick={registerModal.onClose}
+            className="text-neutral-800 cursor-pointer hover:underline"
+          >
+            Log in
+          </div>
         </div>
-
       </div>
     </div>
   );
---
+++ app/types/index.ts
@@ -0,0 +1,8 @@
+import { User } from "@prisma/client";
+
+export type SafeUser = Omit<User, "createAt" | "updatedAt" | "emailVerified"> & {
+    createdAt: string;
+    updatedAt: string;
+    emailVerified: string | null;
+};
+

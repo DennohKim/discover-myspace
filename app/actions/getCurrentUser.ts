391ab0bd79a0e1ac99a7293ee2af7d0954265509
dennohkim <kimathidennis38@gmail.com>
2023-04-06 13:48:30 +0300

    Fix: correct typo

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -12,7 +12,9 @@ export default async function getCurrentUser() {
     try {
         const session = await getSession();
 
-        if(!session?.user?.email) return null;
+        if(!session?.user?.email) {
+            return null;
+        };
 
         const currentUser = await prisma.user.findUnique({
             where: {
@@ -20,10 +22,15 @@ export default async function getCurrentUser() {
             }
         });
 
-        if(!currentUser) return null;
+        if(!currentUser) {
+            return null;
+        };
 
         return {
-            ...currentUser, createdAt: currentUser.createdAt.toISOString(), updatedAt: currentUser.updatedAt.toISOString(), emailVerified: currentUser.emailVerified?.toISOString()
+            ...currentUser,
+         createdAt: currentUser.createdAt.toISOString(),
+         updatedAt: currentUser.updatedAt.toISOString(), 
+         emailVerified: currentUser.emailVerified?.toISOString() || null
         }
     } catch (error: any) {
         return null;

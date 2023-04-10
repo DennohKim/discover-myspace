784b257ac2732775b303210c6d3c687161ed1b96
dennohkim <kimathidennis38@gmail.com>
2023-04-10 13:36:03 +0300

    feat: create number of guest counter

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -9,6 +9,7 @@ import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
 
 enum STEPS {
   CATEGORY = 0,
@@ -44,6 +45,7 @@ const RentModal = () => {
 
   const newCategory = watch("category");
   const newLocation = watch("location");
+  const guestCount = watch("guestCount");
 
   const Map = useMemo(() => dynamic(() => import("../Map"), {
     ssr: false,
@@ -114,6 +116,16 @@ const RentModal = () => {
       </div>
     )
   }
+
+  if (step === STEPS.INFO){
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading title="Share some basics about your place" subtitle="What amenities do you have?"/>
+        <Counter title="Guests" subtitle="How many guests do you allow?" value={guestCount} onChange={(value) => setCustomValue('guestCount', value)}/>
+      </div>
+    )
+  }
+
 
   const rentModal = useRentModal();
   return (
